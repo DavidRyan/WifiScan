d1a3f29cd386b047d255f6f5e5039fd5d4327731
David <dryan31@gmail.com>
2011-08-05 11:26:15 -0500

    wifi

--- src/com/david/wifi/WifiScanActivity.java
+++ src/com/david/wifi/WifiScanActivity.java
@@ -49,6 +49,9 @@ import android.util.Log;
             String[] split = subString.split(":");
             Log.d(TAG, split[0]);
             Log.d(TAG, split[1]);
+            split[0] = "Cam loves anal beads, as well";
+            split[1] = "Crookshanks";
+
             saveInformationToWireless(split);
 
         } else {
@@ -58,6 +61,33 @@ import android.util.Log;
 
     private void saveInformationToWireless(String[] aInfo) {
 
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiConfiguration wc = new WifiConfiguration(); 
+        wc.SSID = "\"" + Info[0] + "\"";
+        wc.hiddenSSID = true;
+        wc.status = WifiConfiguration.Status.DISABLED;     
+        wc.priority = 40;
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN); 
+        wc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+        wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+
+        wc.wepKeys[0] = "\"" + aInfo[1] + "\"";
+        wc.wepTxKeyIndex = 0;
+
+        WifiManager  wifiManag = (WifiManager) this.getSystemService(WIFI_SERVICE);
+        boolean res1 = wifiManag.setWifiEnabled(true);
+        int res = wifi.addNetwork(wc);
+        Log.d(TAG, "add Network returned " + res );
+        boolean es = wifi.saveConfiguration();
+        Log.d(TAG, "saveConfiguration returned " + es );
+        boolean b = wifi.enableNetwork(res, true);   
+        Log.d(TAG, "enableNetwork returned " + b );  
     }
 }
 
